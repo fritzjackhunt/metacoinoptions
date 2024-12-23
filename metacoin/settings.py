f1e60100275d927b0ca043eae47094fc50357032
Aniekan Essiet <essiet.aniekan@yahoo.com>
2024-12-23 04:12:43 +0300

    renamed bullhub

--- metacoin/settings.py
+++ metacoin/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'metacoin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'iYbOctKxxbnXFuaYWwQvlQqlIpBJJtZR',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '40979',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
